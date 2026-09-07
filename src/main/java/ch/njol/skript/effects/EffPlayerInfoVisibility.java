1f16ef035663df288082a6168ab06156e7a69ae8
Playr <69977963+playr-lol@users.noreply.github.com>
2026-09-07 10:00:38 -0400

    fix typo

--- src/main/java/ch/njol/skript/effects/EffPlayerInfoVisibility.java
+++ src/main/java/ch/njol/skript/effects/EffPlayerInfoVisibility.java
@@ -15,7 +15,7 @@ import org.jetbrains.annotations.Nullable;
 @Name("Player Info Visibility")
 @Description({"Sets whether all player related information is hidden in the server list.",
 		"The Vanilla Minecraft client will display ??? (dark gray) instead of player counts and will not show the",
-		"<a href='#ExprHoverList'>hover hist</a> when hiding player info.",
+		"<a href='#ExprHoverList'>hover list</a> when hiding player info.",
 		"<a href='#ExprVersionString'>The version string</a> can override the ???.",
 		"Also the <a href='#ExprOnlinePlayersCount'>Online Players Count</a> and",
 		"<a href='#ExprMaxPlayers'>Max Players</a> expressions will return -1 when hiding player info."})
